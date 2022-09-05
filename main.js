b57be8e7b66ed3d765b9de949b5a8e4ceac4b6ac
Paulo Victor <paulo.tspi@gmail.com>
2022-09-05 18:13:56 -0300

    js modules

---
+++ main.js
@@ -0,0 +1,3 @@
+import message from './utils.js' //Nessa linha ele ja importa e já executa o arquivo
+
+console.log(message)
--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 const message = "Mensagem" 
 
-alert(message)
+alert(message) // Essa variável fica isolada nesse arquivo e não consigo usar ela em outro lugar 
 
-import './util.js'
+export default message
